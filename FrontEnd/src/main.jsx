65f7c68630ce3cdeef7af290f6bab57bdde03081
Rachit Shivhare <rachitshivhare@gmail.com>
2025-05-18 00:29:35 +0530

    fix: Wrap application in React.StrictMode for improved development experience

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -4,9 +4,10 @@ import App from "./App" ;
 import {BrowserRouter} from "react-router-dom" ;
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
